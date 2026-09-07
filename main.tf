0c6bdb973367b213690a22ea7d801504b938a9d2
tauqirnasir <96663903+tauqirnasir@users.noreply.github.com>
2026-09-07 15:13:18 -0400

    Add subnet_id to EC2 instance configuration

--- main.tf
+++ main.tf
@@ -16,6 +16,7 @@ module "nexus_instance" {
   ami          = var.aws_ami  # Example AMI ID, replace with your desired AMI
   instance_type = var.aws_type             # Example instance type, replace with your desired type
   instance_name = "nexus"
+  subnet_id     = "subnet-047322bf237728f6a"
 }
 
 module "sonar_instance" {
